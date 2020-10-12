9ab2dec5187b9e30d5cfa8863144d402a8ee0a2e
Alesagal <9695570+Alesagal@users.noreply.github.com>
2020-10-12 18:03:08 +0200

    Diagnostics created when source code cannot be generated.

--- src/BlazorJSInterop.SourceGenerator/CandidateInterfacesProcessor.cs
+++ src/BlazorJSInterop.SourceGenerator/CandidateInterfacesProcessor.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
 using System.Linq;
-using BlazorJSInterop.SourceGenerator.Exceptions;
+using System.Threading.Tasks;
+using BlazorJSInterop.SourceGenerator.Attributes;
+using BlazorJSInterop.SourceGenerator.Diagnostics;
+using BlazorJSInterop.SourceGenerator.Extensions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
@@ -10,13 +13,16 @@ namespace BlazorJSInterop.SourceGenerator
     {
         private readonly INamedTypeSymbol _interfaceAttributeSymbol;
         private readonly INamedTypeSymbol _methodAttributeSymbol;
+        private readonly DiagnosticReporter _diagnosticReporter;
 
         internal CandidateInterfacesProcessor(
             INamedTypeSymbol interfaceAttributeSymbol,
-            INamedTypeSymbol methodAttributeSymbol)
+            INamedTypeSymbol methodAttributeSymbol,
+            DiagnosticReporter diagnosticReporter)
         {
             _interfaceAttributeSymbol = interfaceAttributeSymbol;
             _methodAttributeSymbol = methodAttributeSymbol;
+            _diagnosticReporter = diagnosticReporter;
         }
 
         internal bool IsCandidateInterface(INamedTypeSymbol interfaceSymbol) =>
@@ -33,17 +39,34 @@ namespace BlazorJSInterop.SourceGenerator
             foreach (var method in interfaceDeclarationSyntax.Members)
             {
                 var methodSymbol = model.GetDeclaredSymbol(method) as IMethodSymbol;
+                var interfaceFullName = $"{interfaceSymbol.ContainingNamespace}.{interfaceSymbol.Name}";
+                var methodName = methodSymbol.Name;
 
                 if (methodSymbol.GetAttributes().Any(attributeData =>
                     attributeData.AttributeClass.Equals(_methodAttributeSymbol, SymbolEqualityComparer.Default)))
                 {
                     methodSymbols.Add(methodSymbol);
                 }
-            }
+                else
+                {
+                    _diagnosticReporter.ReportMethodDiagnostic(
+                        DiagnosticType.MethodWithoutAttribute,
+                        methodName,
+                        interfaceFullName,
+                        typeof(BlazorJSInteropMethodAttribute).FullName,
+                        methodSymbol.Locations);
+                }
 
-            if (interfaceDeclarationSyntax.Members.Count != methodSymbols.Count)
-                throw new InterfaceMethodDoesNotHaveAttributeException(
-                    $"{interfaceSymbol.ContainingNamespace}.{interfaceSymbol.Name}");
+                if (!methodSymbol.ReturnType.IsValueTaskType())
+                {
+                    _diagnosticReporter.ReportMethodDiagnostic(
+                        DiagnosticType.MethodInvalidReturnType,
+                        methodName,
+                        interfaceFullName,
+                        typeof(ValueTask).FullName,
+                        methodSymbol.Locations);
+                }
+            }
 
             return new ValidInterfaceInfo(interfaceSymbol.Name, interfaceSymbol.ContainingNamespace.ToString(),
                 methodSymbols);
