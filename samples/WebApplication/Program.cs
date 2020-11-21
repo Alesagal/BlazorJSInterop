caebee56e5ca7e0c1f8a1ffedb91495c7ea7c275
Alesagal <9695570+Alesagal@users.noreply.github.com>
2020-11-21 14:21:28 +0100

    Upgrade sample project.

--- samples/WebApplication/Program.cs
+++ samples/WebApplication/Program.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Net.Http;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Threading.Tasks;
+using System.Text;
 using BlazorJSInterop.SourceGenerator.Extensions;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace WebApplication
 {
@@ -17,10 +21,10 @@ namespace WebApplication
             // Add this line to register the generated code into the dependency injection collection.
             builder.AddJSInteropImplementations(Assembly.GetExecutingAssembly());
 
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddTransient(sp => new HttpClient
-                {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+            builder.Services.AddScoped(
+                sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
             await builder.Build().RunAsync();
         }
