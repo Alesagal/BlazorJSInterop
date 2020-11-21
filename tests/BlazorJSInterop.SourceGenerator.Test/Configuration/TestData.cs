bda41806d1720d86b68edc30a29680d541567e97
Alesagal <9695570+Alesagal@users.noreply.github.com>
2020-11-21 14:04:06 +0100

    Migrate to .NET 5. Add more unit tests.

--- tests/BlazorJSInterop.SourceGenerator.Test/Configuration/TestData.cs
+++ tests/BlazorJSInterop.SourceGenerator.Test/Configuration/TestData.cs
@@ -76,5 +76,38 @@ namespace BlazorJSInterop.SourceGenerator.Test.TestData
         ValueTask ShowNameAlert(string name);
     }
 }";
+
+        internal const string ValidGeneratedSourceCodeText = @"using Microsoft.JSInterop;
+using System.Threading.Tasks;
+using BlazorJSInterop.SourceGenerator.Attributes;
+
+namespace BlazorJSInterop.SourceGenerator.Test.TestData.Generated
+{
+    [GeneratedImplementation(typeof(BlazorJSInterop.SourceGenerator.Test.TestData.IJSService))]
+    public class IJSService__Generated : BlazorJSInterop.SourceGenerator.Test.TestData.IJSService
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public IJSService__Generated(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public ValueTask ShowHelloWorldAlert()
+        {
+            return _jsRuntime.InvokeVoidAsync(""showHelloWorldAlert"");
+        }
+
+        public ValueTask<string> ShowNamePrompt(System.String title)
+        {
+            return _jsRuntime.InvokeAsync<string>(""showNamePrompt"", title);
+        }
+
+        public ValueTask ShowNameAlert(System.String name)
+        {
+            return _jsRuntime.InvokeVoidAsync(""showNameAlert"", name);
+        }
+    }
+}";
     }
 }
